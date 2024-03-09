d523ea72331a1019da2ba045866d74e109f76ccf
rovannoufer <noufermohamed007@gmail.com>
2024-03-10 00:42:31 +0530

    publish to db

--- src/components/publishform.jsx
+++ src/components/publishform.jsx
@@ -4,6 +4,10 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { useContext } from 'react';
 import { EditorContext } from '../pages/editor';
 import { Tag } from './tags';
+import axios from 'axios';
+import { UserContext } from '../App';
+import { useNavigate } from 'react-router-dom';
+import { toolbarClasses } from '@mui/material';
 
 const PublishForm = () =>{
 
@@ -11,6 +15,12 @@ const PublishForm = () =>{
 
     let characterLimit = 200;
     let tagLimit = 10;
+    const serverUrl = "http://localhost:3000";
+
+    let { userAuth : { access_token } } = useContext(UserContext);
+    let navigate = useNavigate();
+
+
     const handleCloseEvent = () =>{
         setEditorState("editor")
 
@@ -50,6 +60,53 @@ const PublishForm = () =>{
             e.target.value = ""
         }
     }
+
+    const publishBlog = (e) =>{
+ 
+        if(e.target.className.includes("disable")){
+            return;
+        }
+
+        if(!title.length){
+            return toast.error("Write Blog Title before publishing.")
+        }
+        if(!des.length || des.length > characterLimit){
+            return toast.error(`You must provide a blog description under ${characterLimit} characters`)
+        }
+        if(!tags.length){
+            return toast.error("There must be some tags for blog")
+        }
+
+        let loadingToast = toast.loading("Publishing..");
+        e.target.classList.add('disable');
+
+
+        let blogObj = {
+            title, banner, des, content, tags, draft: false
+        }
+
+        
+        axios.post(serverUrl + "/create-blog",  blogObj, {
+            headers:{
+                'Authorization' : `Bearer ${access_token}`
+            }
+        })
+        .then(() =>{
+            e.target.classList.remove('disable');
+            toast.dismiss(loadingToast);
+            toast.success("Published");
+            setTimeout(() =>{
+                navigate("/")
+            }, 500);
+        }).catch(( { response } ) =>{
+            e.target.classList.remove('disable');
+            toast.dismiss(loadingToast);
+
+            return toast.error(response.data.error)
+        })
+    }
+
+
 
     return (
         <section className='w-screen min-h-screen grid items-center lg:grid-cols-2 py-16 lg:gap-4'>
@@ -105,6 +162,10 @@ const PublishForm = () =>{
                 </div>
                 <p className='mt-1 text-dark-grey text-sm text-right'>
                      { tagLimit -tags.length } tags left</p>
+
+                <button className='btn-dark px-8' onClick={publishBlog}>
+                           Publish
+                </button>
             </div>
             
         </section>
