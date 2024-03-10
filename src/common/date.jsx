1c73a2240885fd531e7dc17f91e71dfa882beb81
rovannoufer <noufermohamed007@gmail.com>
2024-03-10 10:23:26 +0530

    blogcard

---
+++ src/common/date.jsx
@@ -0,0 +1,14 @@
+
+
+let months =["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+let days=["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+const getDay =(timestamp) => {
+    let date = new Date(timestamp);
+
+
+    return `${date.getDate()} ${months[date.getMonth()]}`
+}
+
+export default getDay;
