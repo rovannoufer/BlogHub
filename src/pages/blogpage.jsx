b73ca11c2a1c92f78c4088b8039024b4bb00c7fc
rovannoufer <noufermohamed007@gmail.com>
2024-03-13 00:30:09 +0530

    fetch blog details

--- src/pages/blogpage.jsx
+++ src/pages/blogpage.jsx
@@ -1,13 +1,44 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+
+export const blogstruc = {
+    title: '',
+    des: '',
+    content: [],
+    tags: [],
+    author: { personal_info: { } },
+    banner: '',
+    publishedAt: ''
+
+}
 
 const BlogPage = () =>{
 
     let { blog_id } = useParams();
+    const [ blog, setBlog ] = useState(blogstruc);
+
+    let { title, content, banner, author: {personal_info:{ fullname, username, profile_img }}, publishedAt} = blog;
+
+    const serverUrl = "http://localhost:3000" ;
+    const fetchBlog= () =>{
+        axios.post(serverUrl +"/get-blog", { blog_id })
+        .then(({ data : { blog } }) => {
+            console.log(blog);
+            setBlog(blog);
+        }).catch(error =>{
+            console.log(error);
+        })
+    }
+
+    useEffect(() =>{
+      fetchBlog();
+    },[])
 
 
     return (
-        <p>this is blog {blog_id}</p>
+        <p>this is blog { title } </p>
     )
 
 }
